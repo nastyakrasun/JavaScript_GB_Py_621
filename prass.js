43a0a891af34f584a301f29cbbce58265c6c9e69
Nastya <nastyakrasun@gmail.com>
2021-04-08 12:34:29 +0300

    ПЗ к уроку 1. Основы языка JavaScript

---
+++ prass.js
@@ -0,0 +1,11 @@
+var Tc = 8;
+var Tf = (9 / 5) * Tc + 32;
+alert (Tf);
+
+
+var admin;
+var name = 'Василий';
+admin = name;
+console.log(admin);
+
+console.log('JS-выражение 1000 + "108" = ' + (1000 + '108'));
